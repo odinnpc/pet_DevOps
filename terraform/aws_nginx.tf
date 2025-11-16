f37bdf8e9e304aad3f876dc9904dc1df86d1db9b
oddin_linux <npcodin@proton.me>
2025-11-16 07:24:09 +0100

    add some info awsnginx

---
+++ terraform/aws_nginx.tf
@@ -0,0 +1,56 @@
+resource "aws_security_group" "nginx_sg" {
+  name        = "nginx_security_group"
+  description = "Allow HTTP inbound"
+
+dynamic "ingress" {
+    for_each = [80, 443]
+
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nginx_security_group"
+  }
+}
+
+resource "aws_instance" "ubuntu_nginx" {
+  #count = 2
+  ami                    = "ami-0fa91bc90632c73c9" # AMI ID for Ubuntu in us-east-1
+  instance_type          = "t3.micro"
+  key_name               = "super_duper"
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+  user_data = templatefile("file_for_terraform.sh.tpl", {
+})
+  tags = {
+    Name = "instance_terraform"
+    #Name = format("instance_terraform_%02d", count.index + 1) # differentiate instances if count > 1
+    owner   = "anton_dev_ops"
+    project = "terraform_lunch"
+  }
+}
+resource "aws_eip" "my_permanent_ip" {
+  instance = aws_instance.ubuntu_nginx.id
+  domain   = "vpc"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
